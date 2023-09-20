a9db9ab4d6f77ef9ca281305e31dee5524b15fed
kamrankhanorakzai <turababu980@gmail.com>
2023-09-20 10:40:40 +0100

    auto type

--- facebook.js
+++ facebook.js
@@ -4,35 +4,7 @@ const v6Elements = document.querySelectorAll('.ri-twitter-fill');
 const v7Elements = document.querySelectorAll('.ri-youtube-fill');
 
 
-// v5.addEventListener('click', function() {
-//     v5.style.color = 'orange';
-//     v5.style.transition = '2s';
-//     setTimeout(() => {
-//         v5.style.color = 'white';
-//         v5.style.transition = '1s';
-//     }, 1000);
-// });
 
-// v6.addEventListener('click', function() {
-//     v6.style.color = '#00acee';
-//     v6.style.transition = '2s';
-//     setTimeout(() => {
-//         v6.style.color = 'white';
-//         v6.style.transition = '1s';
-//     }, 1000);
-// });
-
-// v7.addEventListener('click', function() {
-//     v7.style.color = 'red';
-//     v7.style.transition = '2s';
-//     setTimeout(() => {
-//         v7.style.color = 'white';
-//         v7.style.transition = '1s';
-//     }, 1000);
-// })
-// addClickEvent(v5, 'orange', '2s');
-// addClickEvent(v6, '#00acee', '2s');
-// addClickEvent(v7, 'red', '2s');
 function addClickEventToElements(elements, initialColor, transitionDuration) {
     elements.forEach((element) => {
         element.addEventListener('click', function() {
